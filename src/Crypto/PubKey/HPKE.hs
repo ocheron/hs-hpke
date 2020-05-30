ae59bb3a484289a423169d0b96e2ee53029ba556
Olivier Chéron <olivier.cheron@gmail.com>
2020-05-30 10:24:35 +0200

    Usage explanation

--- src/Crypto/PubKey/HPKE.hs
+++ src/Crypto/PubKey/HPKE.hs
@@ -9,7 +9,39 @@
 -- <https://tools.ietf.org/html/draft-irtf-cfrg-hpke draft-irtf-cfrg-hpke>.
 --
 -- HPKE schemes combine asymmetric and symmetric algorithms in an interoperable
--- standard.
+-- standard to secure communications between a sender (S) and a receiver (R).
+--
+-- The sender prepares an encryption context @ctx@ by selecting a 'KEM', a 'KDF'
+-- and an 'AEAD' encryption algorithm.  In base mode, the context then just
+-- needs the public key of the receiver @pkR@, and optional application-specific
+-- information @info@ that influences symmetric encryption keys.  Context setup
+-- with 'setupBaseS' generates a random ephemeral key, and the corresponding
+-- encapsulated key @enc@ is returned along with the HPKE context:
+--
+-- @
+-- CryptoPassed (enc, ctx) <- 'setupBaseS' kem cipher pkR info
+-- @
+--
+-- When in possession of the encapsulated key @enc@, the receiver calls
+-- 'setupBaseR' to create an equivalent HPKE context with its key pair @(skR,
+-- pkR)@, and the same algorithm parameters that the sender used:
+--
+-- @
+-- let CryptoPassed ctx = 'setupBaseR' kem cipher enc (skR, pkR) info
+-- @
+--
+-- The sender and receiver can then perform one or more AEAD operations 'seal'
+-- and 'open', all bound cryptographically to the context.  The HPKE context
+-- maintains an internal sequence number to generate appropriate nonces.  It is
+-- also possible to derive arbitrary secrets from the context with the 'export'
+-- function.
+--
+-- The base mode is similar to epheremal-static Diffie-Hellman and just needs
+-- the asymmetric key of the receiver.  Other HPKE modes bring additional
+-- authentication with Pre-Shared Key, or sender asymmetric key, or both. Sender
+-- setup is replaced with functions 'setupPSKS', 'setupAuthS' or
+-- 'setupAuthPSKS'.  Receiver setup uses instead functions 'setupPSKR',
+-- 'setupAuthR', or 'setupAuthPSKR'.
 --
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
